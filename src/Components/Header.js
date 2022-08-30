fa21bc5d4ef5cce5c73e9c08d01b79975f565f9c
James <77265615+JamesLakin247@users.noreply.github.com>
2022-08-30 13:20:28 +1000

    commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,8 +6,8 @@ function Header() {
         <header>
             <div className="container">
                 <div className="links-wrapper">
-                    <Link className="nav-link" to="/watchlist">Your Watchlist</Link>
-                    <Link className="nav-link" to="/">Search Page </Link>
+                    <Link className="nav-link" to="movie-watchlist-app/watchlist">Your Watchlist</Link>
+                    <Link className="nav-link" to="movie-watchlist-app/">Search Page </Link>
                 </div>
             </div>
         </header>
